fea2a365245334d845c24cd6082c3856176f6944
Robspierre987 <rmcintyre1698@gmail.com>
2020-02-18 17:44:17 +0000

    Initial Commit

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextPreview = new System.Windows.Forms.RichTextBox();
+            this.richTextDestination = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnReadFile = new System.Windows.Forms.Button();
+            this.btnSaveFile = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // richTextPreview
+            // 
+            this.richTextPreview.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.richTextPreview.Location = new System.Drawing.Point(12, 46);
+            this.richTextPreview.Name = "richTextPreview";
+            this.richTextPreview.ReadOnly = true;
+            this.richTextPreview.Size = new System.Drawing.Size(670, 325);
+            this.richTextPreview.TabIndex = 0;
+            this.richTextPreview.Text = "";
+            // 
+            // richTextDestination
+            // 
+            this.richTextDestination.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.richTextDestination.Location = new System.Drawing.Point(688, 46);
+            this.richTextDestination.Name = "richTextDestination";
+            this.richTextDestination.ReadOnly = true;
+            this.richTextDestination.Size = new System.Drawing.Size(670, 325);
+            this.richTextDestination.TabIndex = 1;
+            this.richTextDestination.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Preview Target File";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(688, 27);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(120, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Preview Destination File";
+            // 
+            // btnReadFile
+            // 
+            this.btnReadFile.Location = new System.Drawing.Point(16, 378);
+            this.btnReadFile.Name = "btnReadFile";
+            this.btnReadFile.Size = new System.Drawing.Size(75, 23);
+            this.btnReadFile.TabIndex = 4;
+            this.btnReadFile.Text = "Read File";
+            this.btnReadFile.UseVisualStyleBackColor = true;
+            this.btnReadFile.Click += new System.EventHandler(this.btnReadFile_Click);
+            // 
+            // btnSaveFile
+            // 
+            this.btnSaveFile.Location = new System.Drawing.Point(688, 378);
+            this.btnSaveFile.Name = "btnSaveFile";
+            this.btnSaveFile.Size = new System.Drawing.Size(75, 23);
+            this.btnSaveFile.TabIndex = 5;
+            this.btnSaveFile.Text = "Save File";
+            this.btnSaveFile.UseVisualStyleBackColor = true;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.DefaultExt = "txt";
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.InitialDirectory = "C:\\";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1384, 476);
+            this.Controls.Add(this.btnSaveFile);
+            this.Controls.Add(this.btnReadFile);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextDestination);
+            this.Controls.Add(this.richTextPreview);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextPreview;
+        private System.Windows.Forms.RichTextBox richTextDestination;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnReadFile;
+        private System.Windows.Forms.Button btnSaveFile;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Security;
+
+
+
+namespace WindowsFormsApplication1
+{
+ 
+
+    public partial class Form1 : Form
+    {
+        // Constants 
+        public const int Frequency = 1;
+        public const int Real = 2;
+        public const int Imaginary = 3;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void SetRichTextDestination(string text)
+        {
+            richTextDestination.Text = text;
+        }
+        
+        private void SetRichTextPreview(string text)
+        {
+            richTextPreview.Text = text;
+        }
+
+        private void SetRichTextDestinationAdd(string text)
+        {
+             richTextDestination.Text += text;
+        }
+
+        private void ReadLineByLine(string fileName)
+        {
+            string line;
+            int counter = 0;
+            string resultLine = "";
+            
+            char delimiter =' ';
+            System.IO.StreamReader file = new System.IO.StreamReader(fileName);
+            System.IO.StreamWriter fileWriter = new StreamWriter("C:/Users/Robert D McI/Source/Repos/Merging-Files/WindowsFormsApplication1/WindowsFormsApplication1/testFile.txt");
+            
+            while ((line = file.ReadLine()) != null)
+            {
+                if (counter == 0)
+                {
+                    SetRichTextDestinationAdd("100\n");
+                    fileWriter.WriteLine("100");
+                }
+                if (counter > 0)
+                {
+                    resultLine = "";
+                    string[] values = line.Split(delimiter);
+                    resultLine += counter + " ";
+                    resultLine += values[Real] + " ";
+                    resultLine += values[Imaginary] + " ";
+                    resultLine += values[Frequency];
+
+                    fileWriter.WriteLine(resultLine);
+
+                    SetRichTextDestinationAdd(resultLine+"\n");
+                }
+                counter++;
+            }
+            fileWriter.Close();
+        }
+
+        private void btnReadFile_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    var sr = new StreamReader(openFileDialog1.FileName);
+                    SetRichTextPreview(sr.ReadToEnd());
+                    string fileLocation = openFileDialog1.FileName;
+                    ReadLineByLine(fileLocation);
+                }
+                catch (SecurityException ex)
+                {
+                    MessageBox.Show($"Security error.\n\nError message: {ex.Message}\n\n" +
+                    $"Details:\n\n{ex.StackTrace}");
+                }
+            }
+        }
+    }
+}
